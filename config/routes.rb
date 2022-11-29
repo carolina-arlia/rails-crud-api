3a3baffa9342c15433e3f58aacf6211ca53f121d
Carolina Arlia <carolinaiarlia@gmail.com>
2022-11-29 12:58:30 +0100

    product new

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'products/new', to: 'products#new'
+  post 'products', to: 'products#create'
 end
